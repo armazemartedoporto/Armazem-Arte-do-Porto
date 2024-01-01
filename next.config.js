52e690ee63fff3ea3d3901d33005e359b32519b1
Tetezw <terezawisser@gmail.com>
2024-01-01 19:03:19 -0300

    refactor: retrict mode

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
+/* eslint-disable quotes */
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  reactStrictMode: true,
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
-  basePath: '/github-pages',
+  basePath: "/github-pages",
 };
+// eslint-disable-next-line no-undef
 module.exports = nextConfig;
