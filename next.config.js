0d616b10ada946752cda58fa60af2ec3b9136a7e
Tetezw <terezawisser@gmail.com>
2024-01-01 16:23:11 -0300

    feat: yml

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const BACKEND_URL = process.env.NODE_ENV === 'production' ? '/next-gh-pages' : "";
 const nextConfig = {
+  output: 'export',
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
+  assetPrefix: BACKEND_URL,
+  basePath: BACKEND_URL,
 };
 module.exports = nextConfig;
