87f365d3f4262ea4a8d7a76489fd2b4b533d995a
Tetezw <terezawisser@gmail.com>
2024-01-01 19:13:18 -0300

    refactor: basePath

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
+  assetPrefix: "/Armazem-Arte-do-Porto",
   basePath: "/Armazem-Arte-do-Porto",
 };
 // eslint-disable-next-line no-undef
