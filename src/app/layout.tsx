42d341faf95123239e88a4090e75607daca434aa
Tetezw <terezawisser@gmail.com>
2024-01-03 21:16:38 -0300

    style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-        <link href="https://fonts.googleapis.com/css2?family=Anton&family=Dancing+Script&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Anton&family=Dancing+Script:wght@500&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>
         <ModaisProvider>
